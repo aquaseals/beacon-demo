5908b478475abfa0b7ec19cf1a5ee3c90f0179e0
aquaseals <itzme.haya@gmail.com>
2025-05-24 23:03:02 -0400

    frquency spectrum working!

--- demo.py
+++ demo.py
@@ -3,62 +3,41 @@ from scipy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 
 from scipy.io import wavfile
-from IPython.display import Audio
-from IPython.core.display import HTML
 import sounddevice as sd
 
-# Set the backend to 'Agg' (non-interactive) or 'TkAgg' (interactive)
-# For Codespaces, use 'Agg' and save the plot to a file or use inline magic if in a notebook.
-import matplotlib
-matplotlib.use('Agg')  # Non-interactive backend (saves to file)
-# OR for Jupyter-like display (if using a notebook in Codespaces):
-# %matplotlib inline
+'''
+amplitude -> volume of a sound, specifically the wave sample of that sound
+frequency -> pitch of a sound, # of sound wave oscillations per second
+
+'''
 
 def play_audio(signal, sample_rate):
-    """
-    Play audio from a NumPy array.
-    Args:
-        signal (np.array): Audio signal (e.g., sine wave).
-        sample_rate (int): Sampling rate in Hz.
-    """
-    # Normalize to 16-bit PCM format (-1 to 1 range)
-    signal_normalized = np.int16(signal * (32767 / np.max(np.abs(signal))))
-    sd.play(signal_normalized, sample_rate)
-    sd.wait()  # Wait until playback finishes
+    #takes floting point # and turns it back into audio waveform samples
+    signal_normalized = np.int16(signal * (32767 / np.max(np.abs(signal)))) 
+    #plays that audio waveform sample at the given sample rate (frequency, hz) one at a time -> multiple samples strung together make the sound
+    sd.play(signal_normalized, sample_rate) 
+    # wait until playback finishes
+    sd.wait()  
 
-
+# return (sample_rate and audio data)
 input_audio = wavfile.read("Tokyo Rain Serenade.wav")
-sample_rate = input_audio[0]
+# how many samples per second
+sample_rate = input_audio[0] 
+print(sample_rate)
 
 channel_0 = input_audio[1][:, 0]
+# converts audio data (repped by long range of #) into managable floating point # between the smallest and largest data # (e.x. if 0.9m is the highest amplitude sample, that becomes 1)
+# specifically extracts only the left channel (appplicable to stereo or headphones for example) -> explains why when playing the sound it no longer has that dimension?
 audio = np.array(channel_0, dtype=np.float64) / np.max(channel_0)
 
-play_audio(audio, sample_rate)
+#play_audio(audio, sample_rate)
+#computes floting point # representation of sample into complex coeffcients (coefficienct represents how loud a specific frequency is -> amplitude, when frequency starts in the sequence of samples)
+fourier_coefficients = np.fft.rfft(audio, norm="forward")
+frequency_spectrum =  np.fft.rfftfreq(len(audio), 1 / sample_rate)
 
+plt.figure(figsize=(20, 5))
+plt.plot(frequency_spectrum, fourier_coefficients.real)
+plt.xlabel("frequency (hz)", fontsize=14)
 
-'''# Generate signal
-sr = 2000  # Sample rate
-ts = 5.0 / sr  # Sample interval
-t = numpy.arange(0, 1, ts)  # Time vector
-freq = 500  # Frequency (Hz)
-x = 3 * numpy.sin(2 * numpy.pi * freq * t)  # Signal
-
-# Compute FFT
-N = len(x)
-yf = fft(x)
-xf = fftfreq(N, ts)
-
-# Plot
-plt.figure(figsize=(10, 6))
-plt.plot(xf, numpy.abs(yf))
-plt.title('FFT of Sine Wave')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude')
-plt.grid()
-
-# Save the plot to a file (since 'Agg' backend doesn't display interactively)
 plt.savefig('fft_plot.png')
 print("Plot saved as 'fft_plot.png'")
-
-# Alternative: Display in-line (if using a Jupyter notebook in Codespaces)
-#plt.show()  # Uncomment if using %matplotlib inline'''
